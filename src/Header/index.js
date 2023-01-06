bb4eb5138210344d00c140cde943f8f6f21375e9
MuhammadAkif <akifmalhi786@gmail.com>
2023-01-07 01:26:32 +0500

    first commit

---
+++ src/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss'
+
+const Header = () => {
+    return(
+        <div className='headerMainOuterWrapper'>
+            <div className=''>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Header
---
+++ src/Screens/LandingPageScreen/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss'
+
+const LandingPageScreen = () => {
+    return(
+        <div className='landingMainOuterWrapper'>
+            <div className=''>
+
+            </div>
+a
+        </div>
+    )
+
+}
+
+export default LandingPageScreen
